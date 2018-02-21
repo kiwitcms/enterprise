ef90eca525cd9bc8fb7681287869756168f183d7
Mr. Senko <atodorov@mrsenko.com>
2018-02-21 11:54:49 +0200

    Enable error reports to Sentry

--- product.py
+++ product.py
@@ -1,4 +1,5 @@
 import os
+import raven
 from .common import *
 
 # Debug settings
@@ -25,3 +26,18 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesSto
 
 # indicate that this is the Enterprise Edition version
 KIWI_VERSION = "%s-ee" % KIWI_VERSION
+
+# enable reporting errors to Setry for easier debugging
+INSTALLED_APPS += ['raven.contrib.django.raven_compat']  # noqa: F405
+
+try:
+    raven_version = "%s-%s" % (KIWI_VERSION, raven.fetch_git_sha(os.path.abspath(os.pardir)))
+except raven.exceptions.InvalidGitRepository:
+    raven_version = KIWI_VERSION
+
+# configuration for Sentry. For now only backend errors are sent to Sentry
+# by default all reports go to Mr. Senko
+RAVEN_CONFIG = {
+    'dsn': 'https://e9a370eba7bd41fe8faead29552f12d7:1417b740821a45ef8fe3ae68ea9bfc8b@sentry.io/277775',  # noqa: E501
+    'release': raven_version,
+}
