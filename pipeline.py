7bb7c72b0cb40a3740b5b2c67cdf52880de9a37d
Mr. Senko <atodorov@mrsenko.com>
2018-08-15 14:27:09 +0300

    Properly search for GitHub emails

--- pipeline.py
+++ pipeline.py
@@ -19,14 +19,14 @@ def email_is_required(strategy, details, backend, user=None, *args, **kwargs):
 def check_if_email_is_in_use(strategy, details, backend, user=None, *args, **kwargs):
     try:
         User.objects.get(email=details['email'])
+
+        messages.error(
+            strategy.request or backend.strategy.request,
+            _("A user with address %(email)s already exists") % {'email': details['email']}
+        )
+        return HttpResponseRedirect(reverse('tcms-login'))
     except User.DoesNotExist:
         pass
-
-    messages.error(
-        strategy.request or backend.strategy.request,
-        _("A user with address %(email)s already exists") % {'email': details['email']}
-    )
-    return HttpResponseRedirect(reverse('tcms-login'))
 
 
 def initiate_defaults(strategy, details, backend, user=None, *args, **kwargs):
