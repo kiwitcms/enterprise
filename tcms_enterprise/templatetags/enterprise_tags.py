8791ead647045992a0f089a3ce48d858376215f2
Mr. Senko <atodorov@mrsenko.com>
2019-05-02 23:05:53 +0300

    Use a new way to redirect GitHub logins
    requires kiwitcms-tenants 0.1.8

--- tcms_enterprise/templatetags/enterprise_tags.py
+++ tcms_enterprise/templatetags/enterprise_tags.py
@@ -13,17 +13,20 @@ register = template.Library()
 @register.simple_tag
 def next_url(request):
     """
-        Return a full URL when working with tenants or
-        a path when tcms_tenants is not installed.
+        Return a URL which will faciliate GitHub login for tenants
+        (avoiding request_uri mismatch) or a regular path when
+        tcms_tenants is not installed.
 
         Used for the ?next= parameter of PSA URLs.
     """
+    next = request.GET.get('next', '/')
+
     if os.environ.get('KIWI_DB_ENGINE', '').find('postgresql') > -1:
         try:
-            from tcms_tenants import utils
+            from tcms_tenants import urls
 
-            return utils.tenant_url(request, connection.schema_name)
+            next = reverse('tcms_tenants:redirect-to', args=[connection.schema_name, next])
         except ImportError:
             pass
-
-    return request.GET.get('next', '/')
+    # handle double slashes in case we want to redirect to tenant's root
+    return next.replace('//', '/')
