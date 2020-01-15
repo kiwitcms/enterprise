bd2fd5a78603cbf86977b1477e72e65ebe9e4d52
Mr. Senko <atodorov@mrsenko.com>
2020-01-15 14:07:27 +0200

    ugettext -> gettext for Django 3

--- local_settings.py
+++ local_settings.py
@@ -4,7 +4,7 @@ import dj_database_url
 
 from django.conf import settings
 from django.urls import reverse_lazy
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 # update DB connection string from the DATABASE_URL environment variable
--- tcms_enterprise/pipeline.py
+++ tcms_enterprise/pipeline.py
@@ -3,7 +3,7 @@ import uuid
 from django.urls import reverse
 from django.contrib import messages
 from django.http import HttpResponseRedirect
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from tcms.utils.permissions import initiate_user_with_default_setups
 
