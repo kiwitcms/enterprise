4b9aeffcdd74fd742c22f4651f865e53dccb218b
Mr. Senko <atodorov@mrsenko.com>
2020-03-15 16:34:51 +0200

    Unconditionally use tenant_url template tag
    because we've already decided that this distribution will be
    MT/Postgres only and everything is enabled unconditionally it
    doesn't make sense for the existing wrappers anymore.

--- tcms_enterprise/templatetags/enterprise_tags.py
+++ tcms_enterprise/templatetags/enterprise_tags.py
@@ -1,8 +1,7 @@
-# Copyright (c) 2019 Alexander Todorov <atodorov@MrSenko.com>
+# Copyright (c) 2019-2020 Alexander Todorov <atodorov@MrSenko.com>
 
 # Licensed under the GPL 3.0: https://www.gnu.org/licenses/gpl-3.0.txt
 
-import os
 from django import template
 from django.db import connection
 from django.urls import reverse
@@ -21,30 +20,8 @@ def next_url(request):
     """
     next = request.GET.get('next', '/')
 
-    if (os.environ.get('KIWI_DB_ENGINE', '').find('postgresql') > -1) and (connection.schema_name != 'public'):
-        try:
-            from tcms_tenants import urls
+    if connection.schema_name != 'public':
+        next = reverse('tcms_tenants:redirect-to', args=[connection.schema_name, next])
 
-            next = reverse('tcms_tenants:redirect-to', args=[connection.schema_name, next])
-        except ImportError:
-            pass
     # handle double slashes in case we want to redirect to tenant's root
     return next.replace('//', '/')
-
-
-@register.simple_tag
-def next_domain(request, schema_name = None):
-    """
-        Return the domain through we are going to redirect if
-        tenants are configured and installed. Otherwise we go
-        directly to the current domain.
-    """
-    if (os.environ.get('KIWI_DB_ENGINE', '').find('postgresql') > -1):
-        try:
-            from tcms_tenants.templatetags.tcms_tenants import tenant_url
-
-            return tenant_url(request, schema_name)
-        except ImportError:
-            pass
-
-    return ''
