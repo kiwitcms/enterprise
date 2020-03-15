462459310d869dfe3191eadadd096ca23c51c88c
Mr. Senko <atodorov@mrsenko.com>
2020-03-15 16:26:09 +0200

    Don't include tcms_tenants.urls
    b/c this is done by the plugin loading code itself

--- tcms_enterprise/urls.py
+++ tcms_enterprise/urls.py
@@ -7,12 +7,3 @@ from tcms.urls import urlpatterns
 urlpatterns += [
     url('', include('social_django.urls', namespace='social')),
 ]
-
-
-# unconditional configuration of multi-tenant URLs
-from tcms_tenants import urls as tcms_tenants_urls
-
-urlpatterns += [
-    url(r'^%stenants/' % os.environ.get('KIWI_TENANTS_URL_PREFIX', ''),
-        include(tcms_tenants_urls, namespace='tenants')),
-]
