0b04025d1348f27f42354efb415bfda5f68e9c73
Mr. Senko <atodorov@mrsenko.com>
2020-11-08 22:11:15 +0200

    Add guardian to AUTHENTICATION_BACKENDS during testing

--- test_settings.py
+++ test_settings.py
@@ -13,6 +13,7 @@ AUTHENTICATION_BACKENDS = [
     'django_python3_ldap.auth.LDAPBackend',
 
     'django.contrib.auth.backends.ModelBackend',
+    'guardian.backends.ObjectPermissionBackend',
 ]
 
 
