ffdc0017353fbc8024082ee6a909a9a2eeaabe08
Mr. Senko <atodorov@mrsenko.com>
2021-01-12 22:11:52 +0200

    Specify mandatory DJANGO_SETTINGS_MODULE for newer pylint-django

--- l10n_settings.py
+++ l10n_settings.py
@@ -5,3 +5,10 @@ SECRET_KEY = 'generate-locales'
 
 BASE_DIR = os.path.dirname(__file__)
 LOCALE_PATHS = [os.path.join(BASE_DIR, 'tcms_enterprise', 'locale')]
+
+# used only for running pylint
+ROOT_URLCONF = 'tcms_enterprise.urls'
+INSTALLED_APPS = [
+    "django.contrib.contenttypes",
+    "attachments",
+]
