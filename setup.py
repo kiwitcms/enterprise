8e634c69da545c8b14c9828ab2758344135574fd
Alexander Todorov <atodorov@otb.bg>
2023-03-29 21:13:26 +0300

    Changelog for v12.1-mt

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def get_install_requires(path):
 
 setup(
     name='kiwitcms-enterprise',
-    version='12.0',
+    version='12.1',
     description='Kiwi TCMS Enterprise Edition',
     long_description=get_long_description(),
     long_description_content_type='text/markdown',
