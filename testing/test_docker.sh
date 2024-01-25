26e24dce4ef33048ac3451dbc5e8b9ac88faf31e
Alexander Todorov <atodorov@otb.bg>
2024-01-25 18:10:09 +0200

    Copy over http tests from upstream

--- testing/test_docker.sh
+++ testing/test_docker.sh
@@ -2,12 +2,44 @@
 
 . /usr/share/beakerlib/beakerlib.sh
 
+HTTPS="https://testing.example.bg:8443"
+
+WRK_DIR=$(mktemp -d ./wrk-logs-XXXX)
+chmod go+rwx "$WRK_DIR"
+
 assert_up_and_running() {
     sleep 10
     # HTTP redirects; HTTPS displays the login page
     rlRun -t -c "curl       -o- http://testing.example.bg:8080/  | grep '301 Moved Permanently'"
-    rlRun -t -c "curl -k -L -o- https://testing.example.bg:8443/ | grep 'Welcome to Kiwi TCMS'"
+    rlRun -t -c "curl -k -L -o- $HTTPS/ | grep 'Welcome to Kiwi TCMS'"
 }
+
+get_dashboard() {
+    rlRun -t -c "curl -k -L -o- -c /tmp/testcookies.txt $1/"
+    CSRF_TOKEN=$(grep csrftoken /tmp/testcookies.txt | cut -f 7)
+    rlRun -t -c "curl -e $1/accounts/login/ -d username=ldap_atodorov -d password=h3llo-w0rld \
+        -d csrfmiddlewaretoken=$CSRF_TOKEN -k -L -i -o /tmp/testdata.txt \
+        -b /tmp/testcookies.txt -c /tmp/login-cookies.txt $1/accounts/login/"
+    rlAssertGrep "<title>Kiwi TCMS - Dashboard</title>" /tmp/testdata.txt
+}
+
+
+exec_wrk() {
+    URL=$1
+    LOGS_DIR=$2
+    LOG_BASENAME=$3
+    EXTRA_HEADERS=${4:-"X-Dummy-Header: 1"}
+
+    WRK_FILE="$LOGS_DIR/$LOG_BASENAME.log"
+
+    wrk -d10s -t4 -c4 --script ./testing/print-non-limited-requests.lua -H "$EXTRA_HEADERS" "$URL" > "$WRK_FILE"
+
+    COMPLETED_REQUESTS=$(grep 'non-429 request status=' "$WRK_FILE" | wc -l)
+
+    # this is the number of all completed requests across 10 seconds
+    echo "$COMPLETED_REQUESTS"
+}
+
 
 rlJournalStart
     rlPhaseStartTest "Sanity test - boot the docker image"
@@ -115,8 +147,88 @@ rlJournalStart
         rlRun -t -c "robot testing/keycloak.robot"
     rlPhaseEnd
 
+    rlPhaseStartTest "Should send ETag header"
+        rlRun -t -c "curl -k -D- $HTTPS/static/images/kiwi_h20.png 2>/dev/null | grep 'ETag'"
+    rlPhaseEnd
+
+    rlPhaseStartTest "Should NOT send Cache-Control header"
+        rlRun -t -c "curl -k -D- $HTTPS/static/images/kiwi_h20.png 2>/dev/null | grep 'Cache-Control'" 1
+    rlPhaseEnd
+
+    rlPhaseStartTest "Should send X-Frame-Options header"
+        rlRun -t -c "curl -k -D- $HTTPS 2>/dev/null | grep 'X-Frame-Options: DENY'"
+    rlPhaseEnd
+
+    rlPhaseStartTest "Should send X-Content-Type-Options header"
+        rlRun -t -c "curl -k -D- $HTTPS 2>/dev/null | grep 'X-Content-Type-Options: nosniff'"
+    rlPhaseEnd
+
+    rlPhaseStartTest "Should send Content-Security-Policy header"
+        rlRun -t -c "curl -k -D- $HTTPS 2>/dev/null | grep $'Content-Security-Policy: script-src \'self\' cdn.crowdin.com;'"
+    rlPhaseEnd
+
+    rlPhaseStartTest "Should send uploads with exactly 1 'Content-Type: text/plain' header"
+        # copy test file externally b/c Kiwi TCMS v12.2 will prevent its upload
+        rlRun -t -c "docker exec -i web /bin/bash -c 'mkdir -p /Kiwi/uploads/attachments/auth_user/2/'"
+        rlRun -t -c "docker cp testing/ldap.py web:/Kiwi/uploads/attachments/auth_user/2/"
+        rlRun -t -c "curl -k -D- $HTTPS/uploads/attachments/auth_user/2/ldap.py 2>/dev/null | grep 'Content-Type: text/plain'"
+
+        CT_HEADER_COUNT=$(curl -k -D- $HTTPS/uploads/attachments/auth_user/2/ldap.py 2>/dev/null | grep -c 'Content-Type:')
+        rlAssertEquals "There should be only 1 Content-Type header" "$CT_HEADER_COUNT" 1
+    rlPhaseEnd
+
+    rlPhaseStartTest "Requests to /accounts/register/ are rate limited"
+        COMPLETED_REQUESTS=$(exec_wrk "$HTTPS/accounts/register/" "$WRK_DIR" "register-account-page")
+        rlLogInfo "COMPLETED_REQUESTS=$COMPLETED_REQUESTS"
+        rlAssertGreaterOrEqual ">= 10 r/s" "$COMPLETED_REQUESTS" 100
+        rlAssertLesserOrEqual  "<= 20 r/s" "$COMPLETED_REQUESTS" 200
+    rlPhaseEnd
+
+    rlPhaseStartTest "Requests to /accounts/login/ are rate limited"
+        COMPLETED_REQUESTS=$(exec_wrk "$HTTPS/accounts/login/" "$WRK_DIR" "login-page")
+        rlLogInfo "COMPLETED_REQUESTS=$COMPLETED_REQUESTS"
+        rlAssertGreaterOrEqual ">= 10 r/s" "$COMPLETED_REQUESTS" 100
+        rlAssertLesserOrEqual  "<= 20 r/s" "$COMPLETED_REQUESTS" 200
+    rlPhaseEnd
+
+    rlPhaseStartTest "Requests to /accounts/passwordreset/ are rate limited"
+        COMPLETED_REQUESTS=$(exec_wrk "$HTTPS/accounts/passwordreset/" "$WRK_DIR" "password-reset-page")
+        rlLogInfo "COMPLETED_REQUESTS=$COMPLETED_REQUESTS"
+        rlAssertGreaterOrEqual ">= 10 r/s" "$COMPLETED_REQUESTS" 100
+        rlAssertLesserOrEqual  "<= 20 r/s" "$COMPLETED_REQUESTS" 200
+    rlPhaseEnd
+
+    rlPhaseStartTest "Requests for static files are NOT rate limited"
+        COMPLETED_REQUESTS=$(exec_wrk "$HTTPS/static/images/kiwi_h20.png" "$WRK_DIR" "static-image")
+        rlLogInfo "COMPLETED_REQUESTS=$COMPLETED_REQUESTS"
+        rlAssertGreaterOrEqual ">= 1000 r/s" "$COMPLETED_REQUESTS" 10000
+    rlPhaseEnd
+
+    rlPhaseStartTest "Requests for /favicon.ico are NOT rate limited"
+        COMPLETED_REQUESTS=$(exec_wrk "$HTTPS/favicon.ico" "$WRK_DIR" "favicon")
+        rlLogInfo "COMPLETED_REQUESTS=$COMPLETED_REQUESTS in 10 seconds"
+        rlAssertGreaterOrEqual ">= 1000 r/s" "$COMPLETED_REQUESTS" 10000
+    rlPhaseEnd
+
+    rlPhaseStartTest "Requests for robots.txt are NOT rate limited"
+        COMPLETED_REQUESTS=$(exec_wrk "$HTTPS/robots.txt" "$WRK_DIR" "robots-txt")
+        rlLogInfo "COMPLETED_REQUESTS=$COMPLETED_REQUESTS"
+        rlAssertGreaterOrEqual ">= 1000 r/s" "$COMPLETED_REQUESTS" 10000
+    rlPhaseEnd
+
+    rlPhaseStartTest "Authenticated requests to / are NOT rate limited"
+        # login and create the cookies file
+        get_dashboard "$HTTPS"
+
+        SESSION_ID=$(grep sessionid /tmp/login-cookies.txt | cut -f 7)
+        COMPLETED_REQUESTS=$(exec_wrk "$HTTPS/" "$WRK_DIR" "dashboard" "Cookie: sessionid=$SESSION_ID")
+        rlLogInfo "COMPLETED_REQUESTS=$COMPLETED_REQUESTS"
+        rlAssertGreaterOrEqual ">= 50 r/s" "$COMPLETED_REQUESTS" 500
+    rlPhaseEnd
+
     rlPhaseStartCleanup
         rlRun -t -c "docker kill keycloak_server"
+        rlRun -t -c "docker-compose -f docker-compose.testing logs --no-color > docker.log"
         rlRun -t -c "docker-compose -f docker-compose.testing down"
         if [ -n "$ImageOS" ]; then
             rlRun -t -c "docker volume rm enterprise_db_data"
