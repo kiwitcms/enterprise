c7aa7b7cee163046fe84d69fef247c7f34052ec2
Alexander Todorov <atodorov@otb.bg>
2025-10-30 13:56:49 +0200

    Use a different way to override the password reset page
    this should not result in an infinite loop

--- tcms_enterprise/root_urls.py
+++ tcms_enterprise/root_urls.py
@@ -4,22 +4,32 @@
 # https://www.gnu.org/licenses/agpl-3.0.html
 
 from django.urls import re_path
+from django.conf import settings
 from django.conf.urls import include
+from django.views.defaults import permission_denied
 from django.views.generic import RedirectView
 
 from tcms.urls import urlpatterns
 from tcms_enterprise import views
 
+
+url_overrides = [
+    re_path(
+        r"^admin/login/",
+        RedirectView.as_view(url="/accounts/login/", permanent=True),
+    ),
+    # override b/c pwd login can be disabled
+    re_path(r"^accounts/login/", views.LoginView.as_view()),
+]
+
+# WARNING: overrides all password reset pages (form, done, confirm, complete)
+# b/c we're matching the beginning of the URL path !
+if not settings.PASSWORD_LOGIN_ENABLED:
+    url_overrides.append(re_path(r"^accounts/passwordreset/", permission_denied))
+
+
 urlpatterns = (
-    [
-        re_path(
-            r"^admin/login/",
-            RedirectView.as_view(url="/accounts/login/", permanent=True),
-        ),
-        # override these b/c they can be disabled
-        re_path(r"^accounts/login/", views.LoginView.as_view()),
-        re_path(r"^accounts/passwordreset/", views.PasswordResetView.as_view()),
-    ]
+    url_overrides
     + urlpatterns
     + [
         re_path(r"", include("social_django.urls", namespace="social")),
