c8e1f52f7fa663d08f6ca53ab6b0c40b1ea420c3
Alexander Todorov <atodorov@otb.bg>
2026-04-28 23:12:12 +0300

    Provide sample SSL configuration for Postgres
    - configuration is done via /docker-entrypoint-initdb.d/
    - for existing servers enable it manually
    - to refresh certificates copy the new ones under $PGDATA

--- testing/runner.sh
+++ testing/runner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) 2024 Alexander Todorov <atodorov@otb.bg>
+# Copyright (c) 2024-2026 Alexander Todorov <atodorov@otb.bg>
 #
 # Licensed under GNU Affero General Public License v3 or later (AGPLv3+)
 # https://www.gnu.org/licenses/agpl-3.0.html
@@ -18,6 +18,20 @@ echo "**** DEBUG, /tmp/kc.env ****"
 cat /tmp/kc.env
 echo "**** END DEBUG ****"
 echo "**** DONE STARTING KEYCLOAK ****"
+
+# Generate SSL certificates for Postgres
+# WARNING: 644 permissions so that initdb.d/ can read them
+# these files are ignored afterwards
+/usr/bin/sscg --verbose --force    \
+    --country BG --locality Sofia  \
+    --organization "Kiwi TCMS"     \
+    --organizational-unit "Quality Engineering" \
+    --hostname database.example.kiwi        \
+    --ca-file       ./postgres-initdb.d/ca.crt        \
+    --cert-file     ./postgres-initdb.d/database.crt  \
+    --cert-mode=0644   \
+    --cert-key-file ./postgres-initdb.d/database.key \
+    --cert-key-mode=0644
 
 # execute test scripts
 ./testing/test_upstream_nginx_config.sh
